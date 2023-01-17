c31a8333162c26ecb4b82c8550fb4cdf716de7ad
gasolina90 <moralerick90@gmail.com>
2023-01-17 16:36:21 -0700

    modified file

--- Abstraction_prep/Program.cs
+++ Abstraction_prep/Program.cs
@@ -4,25 +4,38 @@ public class Program{
 
     // Main() function to serve as entry point for dotnet
     static void Main(){
-        // Create 2 instances of the Job class
-        Job job1 = new Job();
-        Job job2 = new Job();
+        // // Create 2 instances of the Job class
+        // Job job1 = new Job();
+        // Job job2 = new Job();
         
-        // Assign variables to job1 instance
-        job1.comp_name = "Honda";
-        job1.job_title = "Lead ECU Engineer";
-        job1.start_yr = 2020;
-        job1.end_yr = 2022;
+        // // Assign variables to job1 instance
+        // job1.comp_name = "Honda";
+        // job1.job_title = "Lead ECU Engineer";
+        // job1.start_yr = 2020;
+        // job1.end_yr = 2022;
 
-        // Assign variables to job2 instance
-        job2.comp_name = "Vista High School";
-        job2.job_title = "CNS Student Assistant";
-        job2.start_yr = 2012;
-        job2.end_yr = 2016;
+        // // Assign variables to job2 instance
+        // job2.comp_name = "Vista High School";
+        // job2.job_title = "CNS Student Assistant";
+        // job2.start_yr = 2012;
+        // job2.end_yr = 2016;
 
-        // call the display method for both instances
-        job1.display_job();
-        job2.display_job();
+        // // call the display method for both instances
+        // job1.display_job();
+        // job2.display_job();
+
+
+        // Create a new instance of the Resume Class
+        Resume myResume = new Resume();
+
+        // Assign variables to myResume attributes
+        myResume.name = "Erick Morales";
+        myResume.jobs.Add("CNS Student Assistant");
+        myResume.jobs.Add("Jonda Tenchician");
+        myResume.jobs.Add("Bony Audio Sales Representative");
+
+        // Call the myResume display method 
+        myResume.display_resume();
     }
 
 }
