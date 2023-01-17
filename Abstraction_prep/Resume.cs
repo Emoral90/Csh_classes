0e8a0e17917670e936c0d73cadecbb62ab90ff01
gasolina90 <moralerick90@gmail.com>
2023-01-17 16:36:31 -0700

    modified file

--- Abstraction_prep/Resume.cs
+++ Abstraction_prep/Resume.cs
@@ -8,8 +8,14 @@ public class Resume{
     public string name;
     public List<string> jobs;
 
+    public Resume(){}
+
     public void display_resume(){
-        Console.WriteLine($"Name: {name}  Jobs: {jobs}");
+        Console.WriteLine($"Name: {name}  Jobs:");
+
+        foreach (string i in jobs){
+            Console.WriteLine(i);
+        }
     }
 
 }
