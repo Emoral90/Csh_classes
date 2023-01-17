15ef2068238de8a248ea9d4bd7751f35b4bd2115
gasolina90 <moralerick90@gmail.com>
2023-01-17 15:40:17 -0700

    modified file

--- Abstraction_prep/Program.cs
+++ Abstraction_prep/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/************************************************
+*
+*   Practice creating and using classes in C#
+*
+*************************************************/
+public class Job{
+    public string comp_name = "Ibishu";
+    public string job_title = "Lead System Architect";
+    public int start_yr = 2010;
+    public int end_yr = 2020;
+
+    public Job(){
+        // Job job = new Job();
+    }
+
+    public void display_job(){
+        Console.WriteLine($"Job title: {job_title}  Company: {comp_name}  {start_yr}-{end_yr}");
+    }
+    static void Main(){
+        Job job = new Job();
+        
+        job.display_job();
+    }
+
+}
